198cba5d3a0af0bab1ec05bf83e0fdb28f40a1c7
AlejoDuarte23 <alejandro7899871776@gmail.com>
2026-05-10 19:47:42 -0500

    fix: preserve rebar position numbers

--- app/worker/RebarWorker.py
+++ app/worker/RebarWorker.py
@@ -100,6 +100,7 @@ def create_element(build_ele, doc) -> CreateElementResult:
             elements,
             [],
             None,
+            False,
         )
 
         _log("CreateElements finished.")
